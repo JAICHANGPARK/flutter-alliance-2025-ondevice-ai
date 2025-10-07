5fd0b88fe860d7cb367c9c85464ccaa9a28cbb52
Dreamwalker <aristojeff@gmail.com>
2025-10-07 19:43:55 +0900

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,10 @@ android {
         versionName = flutter.versionName
     }
 
+    aaptOptions {
+        noCompress("tflite", "bin", "litertlm")
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
